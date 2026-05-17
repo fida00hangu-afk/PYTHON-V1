c58b51b4943bf1e2365f5847d7af9367dc09a526
fida00hangu-afk <fida00hangu@gmail.com>
2026-05-17 17:32:38 +0500

    git commit -m "add file in folder"
    This is a chapter on the fundamentals of Python, and it also includes small examples.

---
+++ python/pythoneChapterone.py
@@ -0,0 +1,16 @@
+name = "ertugrul fan"
+age = 17
+
+print(name + "\n" + str(age))
+
+print(name * 3)
+
+print("my name is" + name + " "  + str(age))
+
+
+userInput = input("pleas enter your country name")
+print(userInput + "\n" * 3)
+city = input("enter your city name")
+print(city + "\n" * 3)
+print(city + " ")
+
